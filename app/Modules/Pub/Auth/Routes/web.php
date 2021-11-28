8128739e1c80be3cba7bd878f594ec877e63bd40
vlazaay <vladyslav.zaiets@gmail.com>
2021-11-29 00:40:19 +0200

    added AdminLTE and login form

--- app/Modules/Pub/Auth/Routes/web.php
+++ app/Modules/Pub/Auth/Routes/web.php
@@ -1,5 +1,5 @@
 <?php
 
 Route::group(['prefix' => 'auths', 'middleware' => []], function () {
-    Route::get('/login', 'LoginController@showLoginForm')->name('auths.login');
+    Route::get('/login', 'LoginController@showLoginForm')->name('login');
 });
---
+++ resources/views/Pub/layouts/layout.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>AdminLTE 3 | Dashboard</title>
+
+    <!-- Google Font: Source Sans Pro -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
+    <!-- Ionicons -->
+    <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+    <!-- Tempusdominus Bootstrap 4 -->
+    <link rel="stylesheet" href="{{asset('plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css')}}">
+    <!-- Theme style -->
+    <link rel="stylesheet" href="{{asset('dist/css/adminlte.min.css')}}">
+</head>
+<body class="hold-transition sidebar-mini layout-fixed">
+<div class="page-content">
+
+{{--    <!-- Preloader -->--}}
+{{--    <div class="preloader flex-column justify-content-center align-items-center">--}}
+{{--        <img class="animation__shake" src="dist/img/AdminLTELogo.png" alt="AdminLTELogo" height="60" width="60">--}}
+{{--    </div>--}}
+
+
+    <!-- Content Wrapper. Contains page content -->
+    <div class="wrapper">
+        @yield("content")
+    </div>
+    <!-- /.content-wrapper -->
+    @include("Pub::layouts.parts.footer")
+
+</div>
+</body>
+</html>
